1c1bb3d0010b11f5dc990f96c12b0f75f15f4148
Gollubeff <gollubeff@gmail.com>
2025-10-09 18:58:54 +0300

    Сделал поиск по городу, хук фетч погоды и отображение погоды

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import { useState } from "react";
+import Search from "./components/Search/Search";
+import WeatherCard from "./components/WeatherCard/WeatherCard";
+import useFetchWeather from "./helpers/hooks/useFetchWeather";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [city, setCity] = useState("");
 
+  const { weatherData, isLoading, error } = useFetchWeather({ city });
+
+  const handleSearch = (newCity) => {
+    setCity(newCity);
+  };
   return (
     <>
-      <input type="text" />
+      <h1>Погода</h1>
+      <Search onSearch={handleSearch} />
+      {isLoading && <p>Загрузка...</p>}
+      {error && <p>Ошибка: {error.message}</p>}
+      {weatherData && <WeatherCard data={weatherData} />}
     </>
   );
 }
