e7c6c11c0bac71d86b24c8fcc0899b50882ccc8b
Gollubeff <gollubeff@gmail.com>
2025-10-09 17:33:19 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <input type="text" />
+    </>
+  );
+}
+
+export default App;
